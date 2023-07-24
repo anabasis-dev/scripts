c4edab76b3beb3a5d39420138b6c7969d146a6af
Anabasis <136603493+anabasis-global@users.noreply.github.com>
2023-07-24 23:18:13 +0300

    Create autonmap.sh

---
+++ autonmap.sh
@@ -0,0 +1,11 @@
+
+# ports:
+# 9100 = the RAW port for most printers, also known as the direct-IP port
+# 515 = the LPR/lpd port, for most printers, as well as older print-servers
+# 631 = the IPP port, for most modern printers, and CUPS-based print-server
+
+ports_print=("9100" "515" "631")
+
+os_ports=("139")
+
+
